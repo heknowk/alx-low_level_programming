8c05841e778fca82403cb52cface07dbd3127851
heknowk <engidazer@gmail.com>
2023-09-13 00:06:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+
+	}
+	_putchar('\n');
+
+}
