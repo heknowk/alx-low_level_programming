7c4d08a7dec90767f1b20f126b287b8d13d5140f
heknowk <engidazer@gmail.com>
2023-10-26 06:41:58 -0700

    m

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,29 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * print_binary - prints binary num
- * @n: the num
- * Return: void
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
-	int print = 0;
+	unsigned long int temp;
+	int shifts;
 
 	if (n == 0)
-		_putchar('0');
-	for (i = 1UL << 63; i > 0; i >>= 1)
 	{
-		if (n & i)
-		{
-			_putchar('1');
-			print = 1;
-		}
-		else if (print)
-			_putchar('0');
+		printf("0");
+		return;
+	}
 
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
 	}
 }
